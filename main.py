b78528713fdb7b752cdf9ecb05fc6d72ed1ee23c
paulotgc <paulotgc@gmail.com>
2025-04-25 13:19:05 -0300

    Enviar mensagens no zapzap com python

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pywhatkit as kit
 from datetime import datetime
 
+# variavel para atualizar a hora
 data_atual = datetime.now()
 
-kit.sendwhatmsg('+5583988288483', 'Seja bem vindo(a) ao meu teste de automação do whatsapp com python.',
+# enviar mensagem automatica depois de 1 minuto
+kit.sendwhatmsg('+5583988288483', 'Seja bem vindo(a) ao meu teste de automação do whatsapp com python, essa é uma mensagem texte.',
                 data_atual.hour,
                 data_atual.minute + 1)
+
+
